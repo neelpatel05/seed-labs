15cb1a81775483c332193f32d46e850aca2e37e5
Neel <neel.patel6573@gmail.com>
2020-06-30 09:59:47 -0400

    changes

--- packet-sniffing/scapy/sniffer.py
+++ packet-sniffing/scapy/sniffer.py
@@ -1,9 +1,21 @@
 from scapy.all import *
 
 def process_packet(packet):
-	packet.show()
-	print("-----------------------------------------------------------------")
+	if packet.haslayer(IP):
+		ip = packet[IP]
+		print("IP {} : {}".format(ip.src, ip.dst))
 
-packet = sniff(iface="ens33", filter="icmp or udp", prn=process_packet)
+	if packet.haslayer(TCP):
+		tcp = packet[TCP]
+		print("TCP src: {}, dst: {}".format(tcp.sport, tcp.dport))
+	elif packet.haslayer(UDP):
+		udp = packet[UDP]
+		print("UDP src: {}, dst: {}".format(udp.sport, udp.dport))
+	elif packet.haslayer(ICMP):
+		icmp = packet[ICMP];
+		print("ICMP type: {}".format(icmp.type))
+	print("-------------------------------------------------------------------------------")
+
+packet = sniff(iface="ens33", filter="ip", prn=process_packet)
 
 packet.summary()
